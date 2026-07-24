deed95df638b820ea60b35981ef25fa956096609
Meherun Nessa Ferdausi <70442441+meheroon@users.noreply.github.com>
2026-07-25 00:20:19 +0600

    Fix jest.config.ts - use setupFiles instead of invalid setupFilesAfterSetup

--- portfolio-next/jest.config.ts
+++ portfolio-next/jest.config.ts
@@ -5,7 +5,7 @@ const createJestConfig = nextJest({ dir: "./" });
 
 const config: Config = {
   testEnvironment: "jsdom",
-  setupFilesAfterSetup: ["<rootDir>/jest.setup.ts"],
+  setupFiles: ["<rootDir>/jest.setup.ts"],
   moduleNameMapper: {
     "^@/(.*)$": "<rootDir>/$1",
   },
